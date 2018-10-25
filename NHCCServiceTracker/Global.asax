57cae6a68a388c55eb8256a53a3861a831500840
Carl Bishop <carl.bishop@ministrybrands.com>
2018-10-25 15:40:03 -0500

    Add project files.

---
+++ NHCCServiceTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NHCCServiceTracker.MvcApplication" Language="C#" %>
---
+++ NHCCServiceTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NHCCServiceTracker.Startup))]
+namespace NHCCServiceTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
